f43a05a237bd501dcd7251e574f41a49c39c353e
abdelrahman1234z <121643801+abdelrahman1234z@users.noreply.github.com>
2022-12-30 02:37:52 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls enter the first number");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("pls enter the Second number");
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int x = start; x <= end; x++)
+            {
+                sum = 0;
+                for (int i = 1; i <= x / 2; i++)
+                {
+                    if (x % i == 0)
+                        sum++;
+                }
+                if (sum == x)
+                    Console.WriteLine(x + "is aperfect number");
+
+
+             }       
+
+        }
+    }
+}
